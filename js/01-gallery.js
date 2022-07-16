d52b7d20f702593c508dbbcec15df066ca04aa91
SiTatyana <sisenorawork@gmail.com>
2022-07-16 19:36:01 +0100

    finish 1-st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
 
-console.log(galleryItems);
+//1.  Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+//2.  Реализация делегирования на div.gallery и получение url большого изображения.
+//3.  Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+//4.  Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+//5.  Замена значения атрибута src элемента < img > в модальном окне перед открытием.Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+    
+import { galleryItems } from './gallery-items.js';
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+    .map((galleryIt) =>
+        `<div class="gallery__item">
+            <a class="gallery__link" href=${galleryIt.original}> 
+                <img class="gallery__image" src =${galleryIt.preview} data-source=${galleryIt.original} alt="${galleryIt.description}"/>
+            </a>
+        </div>`)
+    .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener('click', showImage)
+
+function showImage(event) {
+    event.preventDefault();
+    if (event.target.classList.contains("gallery__image")) {
+            basicLightbox.create(`
+		<img src=${event.target.dataset.source}>
+	`).show()
+            return;
+    }
+}
