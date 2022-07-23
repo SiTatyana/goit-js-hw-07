6208c02e1998feccd3289a9201d7d979a8ee44f1
SiTatyana <sisenorawork@gmail.com>
2022-07-23 13:06:30 +0100

    add option of closing modal window by Esc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -22,34 +22,54 @@ gallery.insertAdjacentHTML("beforeend", markup);
 
 gallery.addEventListener('click', showImage);
 
-function showImage(event) {
-    event.preventDefault();
-    if (event.target.classList.contains("gallery__image")) {
-            basicLightbox.create(`
-		<img src=${event.target.dataset.source}>
-	`).show()
-            return;
-    }
-}
-
-
-
-
-
 // function showImage(event) {
-
 //     event.preventDefault();
-
-//     const instance = basicLightbox.create(`<img src=${event.target.dataset.source}>`);
-
-//     if (event.key === "Escape") {
-//         instance.close();
-//         console.log (event.key)
-//     }
 //     if (event.target.classList.contains("gallery__image")) {
-//         instance.show();
-//         console.log (event)
+//             basicLightbox.create(`
+// 		<img src=${event.target.dataset.source}>
+// 	`).show()
+//             return;
 //     }
-
-//     return;
 // }
+
+
+
+function showImage(event) {
+
+    event.preventDefault();
+
+    const instance = basicLightbox.create(`<img src=${event.target.dataset.source}>`,{	
+        onShow: (instance) => {
+            document.addEventListener('keydown', onEsc)
+        },
+    
+	
+        onClose: (instance) => {
+        document.removeEventListener('keydown',onEsc)
+    }});
+    function onEsc(event) {
+        console.log(event.key)
+     if (event.key === "Escape") {
+        instance.close();
+        console.log (event.key)
+    }
+   
+  }
+    
+if (event.target.classList.contains("gallery__image")) {
+        instance.show();
+        console.log (event)
+    }
+} 
+    
+// Daniil Solodkoy
+// document.addEventListener(
+//     "keydown",
+//     (event) => {
+//       if (event.code !== "Escape") {
+//         return;
+//       }
+//       instance.close();
+//     },
+//     { once: true }
+//   );
